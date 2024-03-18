6e4183046cbb98050e766d068e3ae03597627e58
Sanjeewa Ranuluge <61578403+SRanuluge@users.noreply.github.com>
2024-03-18 20:46:45 +1300

    Update method1.py

--- src/sranuluge/python/find-duplicates/method1.py
+++ src/sranuluge/python/find-duplicates/method1.py
@@ -8,7 +8,7 @@ def is_list_contain_duplicates_1(arr1, arr2):
             if item == nested_item: 
                 return True
     return False
-# O(a * b) Time complexity 
+# O(a * b) Time complexity  
 # O(1)      Space complexity
 
 print(f'Method-01: {is_list_contain_duplicates_1(properties_list_1, properties_list_2)}')
